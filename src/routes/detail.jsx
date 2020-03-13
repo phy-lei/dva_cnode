89ea58c845a283d669fb8c440f027b3d5769eae9
phy-lei <765373325@qq.com>
2020-03-13 16:44:44 +0800

    修改了innerHTML的用法

--- src/routes/detail.jsx
+++ src/routes/detail.jsx
@@ -5,16 +5,20 @@ class Detail extends React.Component{
         super();
         this.detail = React.createRef();
     }
+    //使用这种方式来写入innerHTML
+    createMarkup() {
+      return {__html: this.props.detailData.content};
+    }
     componentDidUpdate(){
         //测试生命周期与dva传值的顺序
         // console.log(this.detail.current.innerHTML);
         // console.log(this.props);
-      this.detail.current.innerHTML=this.props.detailData.content
+      // this.detail.current.innerHTML=this.props.detailData.content
     }
   render(){
       console.log(this.props.detailData);
     return (
-      <div className="detail" ref={this.detail}>
+      <div className="detail" ref={this.detail} dangerouslySetInnerHTML={this.createMarkup()}>
           
       </div>
     );
